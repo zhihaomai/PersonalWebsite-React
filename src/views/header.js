b2576106a7cb6c0ddd6e6cb5da65b220fc3b1f12
Zhihao Mai <zhihaomai@zhihaomai-mbp1.local>
2015-04-05 21:28:31 -0700

    Implemented Header Styling and Buttons

--- src/views/header.js
+++ src/views/header.js
@@ -14,8 +14,53 @@ var Header = React.createClass({
     return (
       <div className="header">
         <ul>
-          <li><Link to="home">Landing</Link></li>
-          <li><Link to="home">Campfire</Link></li>
+          <li className="wordbutton">
+            <Link to="home">
+              <img className="logo" src="../pictures/logo.png"/>
+            </Link>
+          </li>
+          <li className="wordbutton">
+            <Link to="home">Blog</Link>
+          </li>
+          <li className="wordbutton">
+            <Link to="home">Videos</Link>
+          </li>
+          <li className="wordbutton">
+            <Link to="home">Photos</Link>
+          </li>
+          <li className="wordbutton">
+            <Link to="home">Contact</Link>
+          </li>
+          <li className="firstglyphbutton">
+            <a href="http://www.youtube.com/zhihaomai">
+              <i className="fa fa-youtube-square"></i>
+            </a>
+          </li>
+          <li className="glyphbutton">
+            <a href="http://www.twitter.com/zhihaomai">
+              <i className="fa fa-twitter-square"></i>
+            </a>
+          </li>
+          <li className="glyphbutton">
+            <a href="http://www.github.com/zhihaomai">
+              <i className="fa fa-github-square"></i>
+            </a>
+          </li>
+          <li className="glyphbutton">
+            <a href="https://ca.linkedin.com/pub/zhihao-mai/54/603/a91">
+              <i className="fa fa-linkedin-square"></i>
+            </a>
+          </li>
+          <li className="glyphbutton">
+            <a href="http://www.instagram.com/zhihaomai">
+              <i className="fa fa-instagram"></i>
+            </a>
+          </li>
+          <li className="glyphbutton">
+            <a href="http://www.facebook.com/zhihaomai">
+              <i className="fa fa-facebook-square"></i>
+            </a>
+          </li>
         </ul>
       </div>
     );
