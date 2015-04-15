b27b886db255eaa517297d8e3dd228a4d1d371f3
Zhihao Mai <zhihao.z.mai@gmail.com>
2015-04-14 23:58:48 -0700

    Implemented Working Embedded Facebook Video

--- src/views/home.js
+++ src/views/home.js
@@ -10,6 +10,16 @@ var Footer = require('./footer');
 
 var Home = React.createClass({
 
+  componentWillMount: function() {
+    (function(d, s, id) {  
+      var js, fjs = d.getElementsByTagName(s)[0];  
+      if (d.getElementById(id)) return;  
+      js = d.createElement(s); js.id = id;  
+      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";  
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  },
+
 	_onClick: function() {
 		console.log ("HEY");
 	},
@@ -19,9 +29,14 @@ var Home = React.createClass({
       <div className="homePage">
         <Header />
         <div className="content">
-        	<video className="video" width="100%" onClick={this._onClick} autoplay="autoplay">
-  					<source src="https://fbcdn-video-p-a.akamaihd.net/hvideo-ak-xpf1/v/t43.1792-2/10474718_1576963292570527_792381358_n.mp4?efg=eyJxZSI6ImZiY2RuX3ZpZGVvX3JhdGVsaW1pdF9yb2xsb3V0Mix2X3JvbGxvdXQiLCJ2ZW5jb2RlX3RhZyI6ImxlZ2FjeV9oZCJ9&oh=946d9ecdea8cd0f7bf05e29341090cd3&oe=552B7367&__gda__=1428909664_895d8166d9d461724f8ad5b963097b1f" type="video/mp4"></source>
-					</video>
+          <div 
+            style={{maxWidth: 1000, marginTop: 20, marginBottom: 20}} 
+            className="fb-video" 
+            data-allowfullscreen="true" 
+            data-href="https://www.facebook.com/zhifilms/videos/1576962825903907/">
+            <div className="fb-xfbml-parse-ignore">
+            </div>
+          </div>
         	<Footer />
         </div>
       </div>
